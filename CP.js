15757ff3e77fe3bab230bb560fe183f147e1e962
HadhemiJeidi <44751240+HadhemiJeidi@users.noreply.github.com>
2018-11-18 15:28:01 +0100

    Add files via upload

---
+++ CP.js
@@ -0,0 +1,11 @@
+
+const Nom =" Moez"; 
+const Profession ="teacher";
+const age ="25ans" ;
+console.log("Name: " +Nom+ " Profession: " +Profession+ " Age: " +age);
+
+let peaks = ["Tallac", "Ralston", "Rose"]; 
+let canyons = ["Ward", "Blackwood"];
+let tahoe = [...peaks , ...canyons];
+             
+const map1 = tahoe.map();
